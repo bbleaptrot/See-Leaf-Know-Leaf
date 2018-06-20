e651e19f83f3cd474a52b859e526e485f2bb111e
EcZachly <zachwilson130@gmail.com>
2018-06-20 09:41:18 -0700

    making port for django dynamic

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
-
+    import django.core.management.commands.runserver as runserver
+    runserver.DEFAULT_PORT = os.environ['PORT'] or 8000
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)

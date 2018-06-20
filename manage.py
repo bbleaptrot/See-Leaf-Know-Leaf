8ca7c374c9413cc07895ca7284d027e8ffb4159b
EcZachly <zachwilson130@gmail.com>
2018-06-20 09:52:11 -0700

    removing dynamic port

--- manage.py
+++ manage.py
@@ -4,9 +4,5 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
-    import django
-    django.setup()
-    import django.core.management.commands.runserver as runserver
-    runserver.DEFAULT_PORT = os.environ['PORT'] or 8000
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)

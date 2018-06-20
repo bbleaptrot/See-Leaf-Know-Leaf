a71dd2bac6de0d436e6a4a11aebf0bef897c8257
EcZachly <zachwilson130@gmail.com>
2018-06-20 09:42:36 -0700

    actually setting up Django

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "helloworld.settings")
+    import django
+    django.setup()
     import django.core.management.commands.runserver as runserver
     runserver.DEFAULT_PORT = os.environ['PORT'] or 8000
     from django.core.management import execute_from_command_line
